68de056c31b94ba01d04bdd50d39d69321103ea8
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2024-03-09 21:55:50 +0100

    sigma4

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # numerus_perfectus
+
+```
+sigma1
+6
+28
+496
+8128
+Measured time: 53.1678 second(s)
+sigma2
+6
+28
+496
+8128
+Measured time: 29.7022 second(s)
+sigma3
+6
+28
+496
+8128
+Measured time: 28.4608 second(s)
+sigma4
+6
+28
+496
+8128
+Measured time: 5.04254 second(s)
+OK!
+```
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <chrono>
 #include <unordered_map>
+#include <vector>
 
 static std::chrono::time_point<std::chrono::steady_clock> start{};
 static std::chrono::time_point<std::chrono::steady_clock> stop{};
@@ -15,6 +16,42 @@ void stoper_stop() {
     double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(stop - start).count();
     std::cout << "Measured time: " << elapsed_seconds << " second(s)\n";
 }
+
+uint64_t power(uint64_t a, uint64_t b) {
+    if (0 == b) return 1;
+    if (1 == b) return a;
+    return a * power(a, b - 1);
+}
+
+// factorization but slightly different
+// original:
+// 34866 = 2 * 3 * 3 * 13 * 149
+//       = 2 * 3^2   * 13 * 149 <-- exponentation is used to group together
+//                                  multiple copies of the same prime number
+// but this funtion returns
+// 34866 = 2 *   9   * 13 * 149 <-- so they're no longer prime factors, but
+//                                  that's exactly what I need 
+//                                  (for sigma function)
+std::vector<uint64_t> factorish(uint64_t x) {
+    std::vector<uint64_t> result{};
+    for (uint64_t i = 2; i <= x/2; ++i) {
+        if (0 == x % i) {
+            uint64_t foo = 1;
+            for (uint64_t j = 1; j < x; ++j) {
+                if (0 != (x % power(i,j))) {
+                    uint64_t bar = power(i,j-1);
+                    result.push_back(bar);
+                    auto tail = factorish(x/bar);
+                    result.insert(result.end(), tail.begin(), tail.end());
+                    return result;
+                }
+            }
+        }
+    }
+    result.push_back(x);    
+    return result;
+}
+
 
 // naive sigma function
 uint64_t sigma1(uint64_t x) {
@@ -61,11 +98,6 @@ uint64_t sigma3(uint64_t x) {
         for (uint64_t i = 1; i <= x/2; ++i) {
             if (0 == x % i) {
                 acc += i;
-                if (i != 1 && 0 != (x % (i*i))) {
-                    std::cout << "---" << "i=" << i << " x=" << x << " x/i=" << x/i << " acc=" << acc << '\n';
-                    m[x] = sigma3(i) * sigma3(x/i);
-                    return sigma3(i) * sigma3(x/i);
-                }
             }
         }
         acc += x;
@@ -74,7 +106,22 @@ uint64_t sigma3(uint64_t x) {
     }
 }
 
+uint64_t sigma4(uint64_t x) {
+    uint64_t result = 1;
+    auto fac = factorish(x);
+    for (const auto &item : fac) {
+        result *= sigma3(item);
+    }
+    return result;
+}
+
+
 int main() {
+    for (const auto &x : factorish(34866)) {
+        std::cout << x << ", ";
+    }
+    std::cout << '\n';
+    assert((std::vector<uint64_t>{2,9,13,149} == factorish(34866)));
     assert(12 == sigma1(6));
     assert(12 == sigma2(6));
     std::cout << sigma3(8128) << '\n';
@@ -82,7 +129,7 @@ int main() {
     
     std::cout << "sigma1\n";
     stoper_start();
-    for (uint64_t i = 1; i < 10000; ++i) {
+    for (uint64_t i = 1; i < 100000; ++i) {
         if (2 * i == sigma1(i)) {
             std::cout << i << '\n';
         }
@@ -91,7 +138,7 @@ int main() {
 
     std::cout << "sigma2\n";
     stoper_start();
-    for (uint64_t i = 1; i < 10000; ++i) {
+    for (uint64_t i = 1; i < 100000; ++i) {
         if (2 * i == sigma2(i)) {
             std::cout << i << '\n';
         }
@@ -100,8 +147,17 @@ int main() {
 
     std::cout << "sigma3\n";
     stoper_start();
-    for (uint64_t i = 1; i < 10000; ++i) {
+    for (uint64_t i = 1; i < 100000; ++i) {
         if (2 * i == sigma3(i)) {
+            std::cout << i << '\n';
+        }
+    }
+    stoper_stop();
+
+    std::cout << "sigma4\n";
+    stoper_start();
+    for (uint64_t i = 1; i < 100000; ++i) {
+        if (2 * i == sigma4(i)) {
             std::cout << i << '\n';
         }
     }
