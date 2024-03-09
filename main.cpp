92ae83990da7a5d981a81126525a69e404a2ee86
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2024-03-09 13:45:21 +0100

    Introduce naive sigma function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <cassert>
+
+uint64_t sigma(uint64_t x) {
+    uint64_t acc = 0;
+    for (uint64_t i = 1; i <= x; ++i) {
+        if (0 == x % i) {
+            acc += i;
+        }
+    }
+    return acc;
+}
 
 int main() {
-    std::cout << "Hi!\n";
+    assert(12 == sigma(6));
+    for (uint64_t i = 5; i < 8200; ++i) {
+        if (2 * i == sigma(i)) {
+            std::cout << i << '\n';
+        }
+    }
+    std::cout << "OK!\n";
 }
